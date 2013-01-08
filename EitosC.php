ae468cee369f0dfa70769cd1daf9eeba919158f6
eitos <eiege.com@gmail.com>
2013-01-08 19:56:37 +0800

    Signed-off-by: eitos <eiege.com@gmail.com>

---
+++ EitosC.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * 方便工具集调用的入口
+ *
+ * @author gaocheng
+ */
+class EitosC {
+    //put your code here
+}
+
+?>
